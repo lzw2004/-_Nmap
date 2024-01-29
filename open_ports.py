c31348492eb68b23b2ff12ea46fc850b10a12e19
lzw2004 <3206168766@qq.com>
2024-01-29 12:01:48 +0800

    2024-01-29 优化系统信息，开放端口查找

--- open_ports.py
+++ open_ports.py
@@ -8,9 +8,10 @@ def scan_open_ports(host_to_scan, nmap_path):
     nm = nmap.PortScanner(nmap_search_path=(nmap_path,))
     # 使用 Nmap 对象进行扫描，指定扫描参数，这里使用默认的 SYN 扫描
     nm.scan(hosts=host_to_scan, arguments='-p 1-65535 --open')
+
     # 遍历扫描结果，获取开放端口信息
     for host in nm.all_hosts():
-        print("------------------------------------------")
+        print("----------------------------------------------------")
         print(f"主机：{host}")
 
         # 遍历主机的所有协议（例如，TCP、UDP）
@@ -20,7 +21,11 @@ def scan_open_ports(host_to_scan, nmap_path):
             # 获取每个协议下的端口信息
             ports = nm[host][proto].keys()
             for port in ports:
-                print(f"端口: {port} \t 状态: {nm[host][proto][port]['state']}")
-        print("------------------------------------------")
+                print(
+                    f"端口: {port} \t 状态: {nm[host][proto][port]['state']} \t 服务: {nm[host][proto][port]['name']}")
+        print("----------------------------------------------------")
 
-# scan_open_ports('192.168.0.108')
+
+# nmap_path = r"D:\Download_tools\Nmap\nmap.exe"
+#
+# scan_open_ports('192.168.0.114', nmap_path)
--- 测试.py
+++ 测试.py
@@ -1,20 +1,16 @@
 import nmap
 
-
-def get_os_info(host_for_os_info, nmap_path):
-
-    # nm = nmap.PortScanner(nmap_search_path=('nmap', r"D:\Download_tools\Nmap\nmap.exe"))
-    # nmap_path = r"D:\Download_tools\Nmap\nmap.exe"
-
+def get_bug(host_get_bug,nmap_path):
     nm = nmap.PortScanner(nmap_search_path=(nmap_path,))
-    nm.scan(hosts=host_for_os_info, arguments='-O')
-    # 遍历扫描结果，获取操作系统信息
+    nm.scan(hosts=host_get_bug, arguments='-O')
+
+    # print(json.dumps(re, indent=4))
     for host in nm.all_hosts():
         print("------------------------------------------")
         print(f"主机：{host}")
-
+        # os_matches = nm[host]['osmatch']
         if 'osmatch' in nm[host]:
-            if nm[host]['osmatch'] == []:
+            if  nm[host]['osmatch'] == []:
                 print(f"操作系统匹配名称：匹配失败 \n准确度：0%")
             else:
                 os_matches = nm[host]['osmatch']
@@ -27,10 +23,10 @@ def get_os_info(host_for_os_info, nmap_path):
             else:
                 os_classes = nm[host]['osclass']
                 for os_class in os_classes:
-                    print(
-                        f"操作系统类别: {os_class['osfamily']} {os_class['osgen']} \n 准确度: {os_class['accuracy']}%")
+                        print(f"操作系统类别: {os_class['osfamily']} {os_class['osgen']} \n 准确度: {os_class['accuracy']}%")
+
 
 
 nmap_path = r"D:\Download_tools\Nmap\nmap.exe"
-target_host = "192.168.0.104"
-get_os_info(target_host, nmap_path)
+host_get_bug = "192.168.0.1/24"
+get_bug(host_get_bug,nmap_path)
